855e7ca1f42a224d3cfc27629d786fab0ba01f12
thogan <thogan@cadence.com>
2024-06-26 11:53:10 -0400

    work on real crossing algo

--- generator.py
+++ generator.py
@@ -3,26 +3,24 @@ from typing import List
 
 import drawsvg as draw
 import numpy as np
+import shapely
+
+
 
 MinBoardDs = 1000
 MaxBoardDs = 10000
 
 MinDensity = 0.5
 MaxDensity = 1.0
-Designs = 1000
-MaxConnections = 1000
+Designs = 100
+MaxConnections = 100
 
 dx = np.random.randint(MinBoardDs, MaxBoardDs, Designs)
 dy = np.random.randint(MinBoardDs, MaxBoardDs, Designs)
 d = draw.Drawing(MaxBoardDs, MaxBoardDs)
 
 
-class Line:
-    def __init__(self, x0: int, y0: int, x1: int, y1: int):
-        self.x0 = x0
-        self.y0 = y0
-        self.x1 = x1
-        self.y1 = y1
+
 
 
 class Label:
@@ -31,14 +29,28 @@ class Label:
         self.score = score
 
 
-def det_crosses(lines: List[Line]):
+def det_crosses(lines: List[shapely.LineString]):
     total = max(len(lines), 1)
-    crosses = np.random.randint(0, total)
+    already_has_cross = set()
+
+    crosses = 0;
+    for idx in range (total-1):
+        line1 = lines[idx]
+        for jdx in range (idx+1,total):
+            line2 = lines[jdx]
+            if line2 in already_has_cross:
+                continue
+            intersect = line1.intersects(line2)
+            if intersect:
+                crosses += 1
+                already_has_cross.add(line2)
+
+    total = max (total, 1)
     return total, crosses
 
 
 def create_test_cases(labels: List[Label]):
-    os.chdir("/Users/taylorhogan/Documents/Development/goodchip/images")
+    os.chdir("./images")
     for design in range(Designs):
 
         d.clear()
@@ -51,7 +63,8 @@ def create_test_cases(labels: List[Label]):
             xto = np.random.randint(dx[design])
             yfrom = np.random.randint(dy[design])
             yto = np.random.randint(dy[design])
-            aline = Line(xfrom, yfrom, xto, yto)
+            aline = shapely.LineString([(xfrom, yfrom), (xto, yto)])
+
             lines.append(aline)
             line = draw.Line(xfrom, yfrom, xto, yto, stroke='blue', stroke_width=2)
             d.append(line)
@@ -74,7 +87,7 @@ def create_test_cases(labels: List[Label]):
         labels.append(label)
         d.save_svg(filename)
 
-    os.chdir("/Users/taylorhogan/Documents/Development/goodchip")
+    os.chdir(".")
     f = open("labels.csv", "w")
 
     for label in labels:
